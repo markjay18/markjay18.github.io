4dc7ed99de7d3c91eec4977da8df252629d5c387
markjay18 <markjay.lajada@gmail.com>
2017-02-22 19:58:57 -0800

    first website upload

---
+++ profile.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Profile</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" href="image/propic.png" type="image/x-icon" />
+<meta name="keywords" content="SIM, CSS, HTML" />
+<meta name="description" content="SIM MICROFINANCE" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+*{
+	margin:0px;
+	padding:0px;	
+}
+html{
+	background:url('image/img.jpg') no-repeat center center fixed;
+	-webkit-background-size:cover;
+	-moz-background-size:cover;
+	-o-background-size:cover;
+	background-size:cover;
+}
+#header{
+	width:auto;
+	heigth:40px;
+	background:rgba(51, 51, 51, 0.42);
+	color:#7fd8d5;
+	font-family:'arial';
+}
+#prof{
+	width:950px;
+	float:left;
+}
+#picture{
+	text-decoration:none;
+	width:120px;
+	height:120px;
+	background:url('image/pic.png');
+	background-size:120px 120px;
+	background-repeat: no-repeat;
+	border-radius:200px;
+	margin-top:30px;
+	margin-left:200px;
+	flaot:left;
+}
+#name{
+	text-decoration:none;
+	float:right;
+	color:white;
+	font-family:'arial';
+}
+#contact{
+	text-decoration:none;
+	margin-top:180px;
+	margin-left:200px;
+	color:white;
+	font-family:'arial';
+	background:rgba(127, 216, 213, 0.08);
+	width:75%;
+	border-radius:2px;
+}
+#contact p{
+	margin:15px;
+	padding:15px;
+}
+#info{
+	float:right;
+	margin-top:-93px;
+}
+#vetae{
+	text-decoration:none;
+	margin-top:5px;
+	margin-left:200px;
+	color:white;
+	font-family:'arial';
+	background:rgba(127, 216, 213, 0.08);
+	width:75%;
+	border-radius:2px;
+}
+#vetae p{
+	margin:15px;
+	padding:15px;
+}
+</style>
+</head>
+<body>
+<div id="header">
+	<p style="position:absolute; margin-top:19px; margin-left:200px;"><font size="4">Learn how to code with developer</font></p><br><br><br>
+</div>
+
+<div id="prof">
+	<div id="picture"></div>
+		<div id="name">
+			<p style="margin-top:-95px; margin-left:10px;"><h1>Markjay Pantoja Lajada</h1></p>
+			<p style="margin-top:-55px;"><a href="https://www.facebook.com/markjay.lajada"><i class="fa fa-facebook" style="margin-left:600px; font-size:3em; color:#7fd8d5;" onmouseover="this.style.color='rgb(178, 236, 235)'" onmouseout="this.style.color='#7fd8d5'"></i></a></p>
+		</div>
+</div>
+<div id="contact">
+	<p><i class="fa fa-envelope"></i> <font size="3">Email: <a href="https://accounts.google.com/ServiceLogin?sacu=1#identifier">markjay.lajada@gmail.com</a></font><br>
+	<i class="fa fa-phone"></i> <font size="3">Contact No.: 09120122258</font></p>
+<div id="info"><p><i class="fa fa-info" style="font-size:1.7em;" onmouseover="this.style.color='rgb(178, 236, 235)'" onmouseout="this.style.color='white'"></i></div>
+</div>
+<div id="vetae">
+	<font size="5" style="color:#7fd8d5;"><p align="justify">About</font><br>
+	<font size="3" style="color:#white;">I was a student at Marinduque Midwest college, and I graduated with a bachelor's degree in Computer Science. I have knowledge in PHP, CSS and javascript. Going back to my college profession I was able to code a program with my own. It is a mind blowing but as fast as day pass by I start to love programming. This is what I want after learning.</p></font>
+</div>
+</body>
+</html>
